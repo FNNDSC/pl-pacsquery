fadf6ebd6d35671c7902b9e34ccb93d180a8e2e2
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2017-11-08 15:55:55 -0500

    Bump to latest pfurl and version bump 1.0.4

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def readme():
 
 setup(
       name             =   'pacsquery',
-      version          =   '1.0.3',
+      version          =   '1.0.4',
       description      =   'Query a PACS through an intermediary service, "pfdcm" (not included).',
       long_description =   readme(),
       author           =   'Rudolph Pienaar',
